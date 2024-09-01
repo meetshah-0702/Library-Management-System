47223857a37f92380a960e59047795e45b062166
Meet Shah <meetushah999@gmail.com>
2024-09-01 13:31:32 +0530

    Added comments

--- src/Book.java
+++ src/Book.java
@@ -1,11 +1,13 @@
 package src;
 
+// This code represents a book in Library.
 public class Book {
     private String isbn;
     private String title;
     private String author;
     private int publicationYear;
 
+    //This code creates a book with the given details.
     public Book(String isbn, String title, String author, int publicationYear) {
         this.isbn = isbn;
         this.title = title;
--- src/Library.java
+++ src/Library.java
@@ -5,17 +5,21 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 
+//This code represents a library that manages a collection of books.
 public class Library {
     private Map<String, Book> books = new HashMap<>();
 
+    //This method adds a new book to library.
     public void addBook(Book book) {
         books.put(book.getIsbn(), book);
     }
 
+    //This method is used to check whether the book is available or not.
     public boolean isBookAvailable(String isbn) {
         return books.containsKey(isbn);
     }
 
+    //This method is used to borrow a book using its ISBN.
     public void borrowBook(String isbn) {
         if (isBookAvailable(isbn)) {
             books.remove(isbn);
@@ -24,13 +28,14 @@ public class Library {
         }
     }
 
+    //This method is used to return a borrowed book.
     public void returnBook(Book book) {
         books.put(book.getIsbn(), book);
     }
 
+    //This method is used to return all the available books in the library.
     public List<Book> viewAvailableBooks() {
         return new ArrayList<>(books.values());
     }
-
 
 }
